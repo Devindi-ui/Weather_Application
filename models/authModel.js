d50cae7165ba6c850cfca0c62a7dd24a96f9ce09
Devindi-u <panchalidevindi@gmail.com>
2025-08-20 13:45:34 +0530

    authModel

---
+++ models/authModel.js
@@ -0,0 +1,32 @@
+// Auth Model - Handle all authentication related operations
+
+class AuthModel {
+  constructor(auth, database) {
+    this.auth = auth;
+    this.database = database;
+  }
+
+  signup(name, email, password) {
+    return this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const userId = userCredential.user.uid;
+        return this.database.ref("users/" + userId).set({
+          name: name,
+          email: email,
+        });
+      });
+  }
+
+  login(email, password){
+    return this.auth.signInWithEmailPassword(email, password);
+  }
+
+  logout(){
+    return this.auth.signOut();
+  }
+
+  getCurrentUser(){
+    return this.auth.currentUser;
+  }
+}
